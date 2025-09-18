c15ea7bbd22008f148382ed8d9cb8fbfdebbef0e
NaderSameer <nadersameer@std.mans.edu.eg>
2025-09-18 17:41:27 +0300

    Adding Direction to your application

--- lib/core/router/thimar_router.dart
+++ lib/core/router/thimar_router.dart
@@ -1,5 +1,5 @@
 import 'package:go_router/go_router.dart';
-import 'package:thimar/auth/login_view.dart';
+import 'package:thimar/auth/presentation/views/login_view.dart';
 
 class ThimarRouter {
   static const String login = '/';
--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,27 @@ import 'package:thimar/core/router/thimar_router.dart';
 
 void main() {
   runApp(
-     Thimar(thimarRouter:ThimarRouter(),),
+    Thimar(
+      thimarRouter: ThimarRouter(),
+    ),
   );
 }
 
 class Thimar extends StatelessWidget {
   const Thimar({super.key, required this.thimarRouter});
-  final ThimarRouter thimarRouter ; 
+  final ThimarRouter thimarRouter;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       routerConfig: thimarRouter.router,
+      builder: (context, child) {
+        return Directionality(
+          textDirection: TextDirection.rtl,
+          child: child!,
+        );
+      },
     );
   }
 }
