d4492cecbc78f1022debe1217251b251c7f057f3
NaderSameer <nadersameer@std.mans.edu.eg>
2025-09-19 10:12:41 +0300

    Edit in login view

--- lib/feature/auth/presentation/views/login_view.dart
+++ lib/feature/auth/presentation/views/login_view.dart
@@ -10,76 +10,79 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       body: Padding(
         padding: EdgeInsets.all(16),
         child: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              CustomThimarLogo(),
-              SizedBox(
-                height: 16,
-              ),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                CustomThimarLogo(),
+                SizedBox(
+                  height: 16,
+                ),
 
-              Align(
-                alignment: Alignment.centerRight,
-                child: Text(
-                  'نسيت كلمة المرور',
-                  style: ThimarStyle.styleBold16.copyWith(),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    'نسيت كلمة المرور',
+                    style: ThimarStyle.styleBold16.copyWith(),
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 8,
-              ),
-              Text.rich(
-                TextSpan(
-                  children: [
-                    TextSpan(
-                      text:
-                          'أدخل الكود المكون من 4 أرقام المرسل علي رقم الجوال',
-                      style: ThimarStyle.styleLight16,
-                    ),
-                    WidgetSpan(
-                      child: Row(
-                        children: [
-                          Text(
-                            '+9660548745',
-                            textDirection: TextDirection.ltr,
-                            style: ThimarStyle.styleLight16,
-                          ),
-                          TextButton(
-                            onPressed: () {},
-                            child: Text(
-                              'تغيير رقم الجوال',
-                              style: ThimarStyle.styleBold16,
-                            ),
-                          ),
-                        ],
+                SizedBox(
+                  height: 8,
+                ),
+                Text.rich(
+                  TextSpan(
+                    children: [
+                      TextSpan(
+                        text:
+                            'أدخل الكود المكون من 4 أرقام المرسل علي رقم الجوال',
+                        style: ThimarStyle.styleLight16,
                       ),
-                    ),
-                  ],
+                      WidgetSpan(
+                        child: Row(
+                          children: [
+                            Text(
+                              '+9660548745',
+                              textDirection: TextDirection.ltr,
+                              style: ThimarStyle.styleLight16,
+                            ),
+                            TextButton(
+                              onPressed: () {},
+                              child: Text(
+                                'تغيير رقم الجوال',
+                                style: ThimarStyle.styleBold16,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 25,
-              ),
-              CustomPinCodeFilds(),
-              SizedBox(
-                height: 30,
-              ),
-              CustomAuthButton(title: 'تأكيد الكود'),
-              SizedBox(
-                height: 20,
-              ),
-              Align(
-                alignment: Alignment.center,
-                child: Text(
-                  textAlign: TextAlign.center,
-                  'لم تستلم الكود ؟\n يمكنك إعادة إرسال الكود بعد',
-                  style: ThimarStyle.styleLight16,
+                SizedBox(
+                  height: 25,
                 ),
-              ),
-            ],
+                CustomPinCodeFilds(),
+                SizedBox(
+                  height: 30,
+                ),
+                CustomAuthButton(title: 'تأكيد الكود'),
+                SizedBox(
+                  height: 20,
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    textAlign: TextAlign.center,
+                    'لم تستلم الكود ؟\n يمكنك إعادة إرسال الكود بعد',
+                    style: ThimarStyle.styleLight16,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
