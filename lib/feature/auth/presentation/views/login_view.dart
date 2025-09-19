4a17a094ab261c815e1aca9cb9dcdb7bb19b1339
NaderSameer <nadersameer@std.mans.edu.eg>
2025-09-19 14:02:50 +0300

    Refactor and Create a Verfity otp

--- lib/feature/auth/presentation/views/login_view.dart
+++ lib/feature/auth/presentation/views/login_view.dart
@@ -1,128 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:thimar/feature/auth/presentation/widget/custom_auth_button.dart';
-import 'package:thimar/feature/auth/presentation/widget/custom_cirecle_count.dart';
-import 'package:thimar/feature/auth/presentation/widget/custom_pin_code_filds.dart';
-import 'package:thimar/feature/auth/presentation/widget/custom_resend_button.dart';
 import 'package:thimar/feature/auth/presentation/widget/custom_thimar_logo.dart';
-import 'package:thimar/core/style/thimar_style.dart';
 
-class LoginView extends StatefulWidget {
+class LoginView extends StatelessWidget {
   const LoginView({super.key});
 
   @override
-  State<LoginView> createState() => _LoginViewState();
-}
-
-class _LoginViewState extends State<LoginView> {
-  bool isActiveTimer = true;
-  TextEditingController codeController = TextEditingController();
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: true,
-      body: Padding(
-        padding: EdgeInsets.all(20.r),
-        child: SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                CustomThimarLogo(),
-                SizedBox(
-                  height: 16.h,
-                ),
-
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Text(
-                    'نسيت كلمة المرور',
-                    style: ThimarStyle.styleBold16.copyWith(),
-                  ),
-                ),
-                SizedBox(
-                  height: 8.h,
-                ),
-                Text.rich(
-                  TextSpan(
-                    children: [
-                      TextSpan(
-                        text: 'أدخل الكود المكون من 4 أرقام المرسل علي رقم ',
-                        style: ThimarStyle.styleLight16,
-                      ),
-                      WidgetSpan(
-                        child: Row(
-                          children: [
-                            Text(
-                              '+9660548745',
-                              textDirection: TextDirection.ltr,
-                              style: ThimarStyle.styleLight16,
-                            ),
-                            TextButton(
-                              onPressed: () {},
-                              child: Text(
-                                'تغيير رقم الجوال',
-                                style: ThimarStyle.styleBold16,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 25.h,
-                ),
-                CustomPinCodeFilds(
-                  textEditingController: codeController,
-                  onChange: (value) {
-                    setState(() {});
-                  },
-                ),
-                SizedBox(
-                  height: 20.h,
-                ),
-                CustomAuthButton(
-                  title: 'تأكيد الكود',
-                  onPressed: codeController.text.length != 4 ? null : () {},
-                ),
-                SizedBox(
-                  height: 20.h,
-                ),
-                Align(
-                  alignment: Alignment.center,
-                  child: Text(
-                    textAlign: TextAlign.center,
-                    'لم تستلم الكود ؟\n يمكنك إعادة إرسال الكود بعد',
-                    style: ThimarStyle.styleLight16,
-                  ),
-                ),
-                SizedBox(
-                  height: 16.h,
-                ),
-                if (isActiveTimer)
-                  Padding(
-                    padding: EdgeInsets.only(top: 16.h),
-                    child: CustomCirecleCount(
-                      onComplete: () {
-                        isActiveTimer = false;
-                        setState(() {});
-                      },
-                    ),
-                  )
-                else
-                  Center(
-                    child: CustomResendButton(
-                      onPressed: () {
-                        isActiveTimer = true;
-                        setState(() {});
-                      },
-                    ),
-                  ),
-              ],
-            ),
-          ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            CustomThimarLogo(),
+          ],
         ),
       ),
     );
